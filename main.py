265accf57984f11f5ad8bbc636e668df725b88e0
Lynne Munini <muninilynne65@gmail.com>
2021-10-18 18:28:40 -0400

    Initial commit

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from tkinter import *
 # Creating a new window and configurations
 
 window = Tk()
-window.title("Miles to Kilometers Converter")
+window.title("Miles to KM Converter")
 window.minsize(width=300, height=200)
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg="white")
 
 
-entry = Entry(width=15, font=("Courier", 12))
+entry = Entry(width=15, font=("Courier", 12), justify='center', highlightbackground="cyan", bg="white")
 # Add some text to begin with
 entry.insert(END, string="0")
 # Gets text in entry
@@ -16,22 +16,22 @@ entry.grid(column=2, row=2)
 # Label 2
 label2 = Label(text="Miles", font=("Courier", 12))
 label2.grid(column=3, row=2)
-label2.config(padx=30)
+label2.config(padx=30, bg="white")
 
 # Label 3
 label3 = Label(text="is equal to", font=("Courier", 12))
 label3.grid(column=1, row=3)
-label3.config(padx=30)
+label3.config(padx=30, bg="white")
 
 # Label 4
 label3 = Label(text=0)
 label3.grid(column=2, row=3)
-label3.config(padx=30)
+label3.config(padx=30, bg="white")
 
 # Label 5
 label5 = Label(text="Km", font=("Courier", 12))
 label5.grid(column=3, row=3)
-label5.config(padx=30)
+label5.config(padx=30, bg="white")
 
 
 def calculate():
@@ -42,9 +42,9 @@ def calculate():
 
 
 # Button
-button = Button(text="Calculate", font=("Courier", 12, "bold"), command=calculate)
+button = Button(text="Calculate", font=("courier", 12, "bold"), command=calculate)
 button.grid(column=2, row=4)
-button.config(padx=30)
+button.config(padx=30, bg="cyan")
 
 
 window.mainloop()
